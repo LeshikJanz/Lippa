9a5b90093132909d05789863c0b95cdda010bda0
leshikjanz <alex.tereshko@softeq.com>
2018-03-17 14:08:20 +0300

    feat: implement view for footer

--- App.js
+++ App.js
@@ -1,29 +1,20 @@
 // @flow
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native'
-import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
-import createSagaMiddleware from 'redux-saga'
-import rootSaga from "rootSaga"
-// import rootReducer from "./rootReducer"
-
-// const sagaMiddleware = createSagaMiddleware()
-// const store = createStore(
-//   rootReducer,
-//   applyMiddleware(sagaMiddleware)
-// )
-//
-// sagaMiddleware.run(rootSaga)
+import Footer from "components/footer/Footer"
 
 export default class App extends React.Component<null, null> {
   render() {
     return (
-        <View style={styles.container}>
+      <View style={styles.container}>
+        <View>
           <Text>Привет Настя!!!</Text>
           <Text>Open up App.js to start working on your app!</Text>
           <Text>Changes you make will automatically reload.</Text>
           <Text>Shake your phone to open the developer menu.</Text>
         </View>
+        <Footer />
+      </View>
     );
   }
 }
@@ -33,6 +24,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'space-between',
+    marginTop: '5%'
   }
 });
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,20 +1,86 @@
 // @flow
 import React from "react"
-import { TouchableOpacity, View } from "react-native"
-import { styles } from "./styles"
+import { TouchableHighlight, View, Text, StyleSheet, Image } from "react-native"
+import { cameraIcon, galleryIcon, folderIcon, settingsIcon } from "assets"
 
-export const Footer = () =>
-  <View style={styles.container}>
-    <TouchableOpacity
-      style={styles.button}
-      onPress={this.onPress}
-    >1</TouchableOpacity>
-    <TouchableOpacity
-      style={styles.button}
-      onPress={this.onPress}
-    >1</TouchableOpacity>
-    <TouchableOpacity
-      style={styles.button}
-      onPress={this.onPress}
-    >1</TouchableOpacity>
-  </View>
+class Footer extends React.Component<{}, {}> {
+  onPress = (e) => {
+    console.log('onPress')
+    console.log(e)
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TouchableHighlight
+          name="take photo"
+          style={styles.menuItem}
+          onPress={this.onPress}
+        >
+          <Image
+            style={styles.image}
+            source={cameraIcon}
+          />
+        </TouchableHighlight>
+        <TouchableHighlight
+          name="open from library"
+          style={styles.menuItem}
+          onPress={this.onPress}
+        >
+          <Image
+            style={styles.image}
+            source={galleryIcon}
+          />
+        </TouchableHighlight>
+        <TouchableHighlight
+          style={styles.menuItem}
+          onPress={this.onPress}
+        >
+          <Image
+            style={styles.image}
+            source={folderIcon}
+          />
+        </TouchableHighlight>
+        <TouchableHighlight
+          name="settings"
+          style={styles.menuItem}
+          onPress={this.onPress}
+        >
+          <Image
+            style={styles.image}
+            source={settingsIcon}
+          />
+        </TouchableHighlight>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+    backgroundColor: "#3b4142",
+    height: '7%',
+    width: '100%'
+  },
+  menuItem: {
+    width: '25%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRightWidth: 1,
+    borderColor: '#d6d7da'
+  },
+  title: {
+    color: '#fff',
+    fontWeight: 'bold'
+  },
+  image: {
+    width: 40,
+    maxHeight: 35
+  }
+})
+
+
+export default Footer
